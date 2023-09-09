987323ed56a9d7f55f2a0f86112c09f027f2eacf
suru123456 <144535531+suru123456@users.noreply.github.com>
2023-09-09 17:56:19 +0530

    Add files via upload

---
+++ challenge unit1/main (1).py
@@ -0,0 +1,8 @@
+def is leapyear(year)
+if (year% 4==0 and year% 100!=0 or year%400==0:
+return True
+else:
+return Falsetear=int(input("enter the year:")) is leaoyear (year):
+print('s} is a leap year' format(year))
+else
+print('{} is a not a leap year format (year))
